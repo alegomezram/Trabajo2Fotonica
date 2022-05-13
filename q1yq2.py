5375ee5b4aa484b73fce713f555b1cf425ea547e
RaphaelCH <92891280+rcasalins@users.noreply.github.com>
2022-05-13 03:27:14 -0500

    q1q2
    Cambio de escala en el beta, para que no quede tan grande.

--- q1yq2.py
+++ q1yq2.py
@@ -8,11 +8,11 @@ ko = (2*np.pi)/(10 ** -6) #numero de onda para longitud de onda de un micrometro
 #funciones q1 y q2 que contienen la velocidad de propagacion y aparecen en las funciones de Bessel
 
 def q1(beta):
-    q1_equation = np.sqrt((nco ** 2)*(ko ** 2) - (beta ** 2))
+    q1_equation = np.sqrt((nco ** 2)*(ko ** 2) - ((beta * (10 **6)) ** 2))
     return q1_equation
 
 def q2(beta):
-    q2_equation = np.sqrt(-(ncl ** 2)*(ko ** 2) + (beta ** 2))
+    q2_equation = np.sqrt(-(ncl ** 2)*(ko ** 2) + ((beta * (10 **6)) ** 2))
     return q2_equation
 
 
