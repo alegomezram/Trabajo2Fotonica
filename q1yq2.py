3471a7a11e516e8cb5072a890dcce1c621bfca32
RaphaelCH <92891280+rcasalins@users.noreply.github.com>
2022-05-13 00:43:46 -0500

    q1 y q2
    Funciones auxiliares para simplificar las funciones de Bessel, contienen la velocidad de propagación

---
+++ q1yq2.py
@@ -0,0 +1,19 @@
+import numpy as np
+from core_refractive_index import *
+
+nco = n_core() #indice de refracción del nucleo
+ko = (2*np.pi)/(10 ** -6) #numero de onda para longitud de onda de un micrometro
+
+#funciones q1 y q2 que contienen la velocidad de propagacion y aparecen en las funciones de Bessel
+
+def q1(beta):
+    q1_equation = np.sqrt((nco ** 2)*(ko ** 2) - (beta ** 2))
+    return q1_equation
+
+def q2(beta):
+    q2_equation = np.sqrt(-(nco ** 2)*(ko ** 2) + (beta ** 2))
+    return q2_equation
+
+
+
+
