3f4812e4c74e53965f6fe31989eb63adc8a8714f
RaphaelCH <92891280+rcasalins@users.noreply.github.com>
2022-05-13 01:23:47 -0500

    Error q2
    Error en la escritura del indice de refraccion del revestimiento

--- q1yq2.py
+++ q1yq2.py
@@ -2,6 +2,7 @@ import numpy as np
 from core_refractive_index import *
 
 nco = n_core() #indice de refracción del nucleo
+ncl =  1.4527 #indice de refracción del revestimiento
 ko = (2*np.pi)/(10 ** -6) #numero de onda para longitud de onda de un micrometro
 
 #funciones q1 y q2 que contienen la velocidad de propagacion y aparecen en las funciones de Bessel
@@ -11,7 +12,7 @@ def q1(beta):
     return q1_equation
 
 def q2(beta):
-    q2_equation = np.sqrt(-(nco ** 2)*(ko ** 2) + (beta ** 2))
+    q2_equation = np.sqrt(-(ncl ** 2)*(ko ** 2) + (beta ** 2))
     return q2_equation
 
 
