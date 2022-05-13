e8832b380203316d160e0fe1d3cb440a8a96eea8
RaphaelCH <92891280+rcasalins@users.noreply.github.com>
2022-05-13 00:13:25 -0500

    core_refractive_index
    Añadí el calculo del indice de refracción del nucleo, lo importas en otro documento con " from core_refractive_index import * "

---
+++ core_refractive_index.py
@@ -0,0 +1,13 @@
+import numpy as np
+import scipy.special as sp
+
+def n_core():
+    A, wl = [0.6867178, 0.4348151, 0.8965658], [0.0726752, 0.1151435 , 10.002398]
+    n = 0
+    for i in range(0,len(A)):
+        suma = A[i]* (((1300 * (10 ** -9)) ** 2) / (((1300 * (10 ** -9))**2) - ((wl[i] * (10 ** -6))**2)))
+        n = suma + n
+        
+    nco = np.sqrt(1+n)
+        
+    return nco
